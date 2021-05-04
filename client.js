40c2cef187c6465142d0bdfa7aa14b50333efa93
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-05-04 20:46:15 +0000

    added connect funct into client module and export it and require it inside player

--- client.js
+++ client.js
@@ -1,1 +1,28 @@
-// A Separate Module For The TCP Connection
+// client.js is A Separate Module For The TCP Connection
+
+const net = require("net"); //Node's library
+
+// establishes a connection with the game server
+const connect = function() {
+
+  // we used Node's net library and createConnection function to create obj named conn
+  // conn obj represents the connection that you have with the server
+  const conn = net.createConnection({  
+    host: "10.0.2.15", // IP address here
+    port: "50541" // PORT number here
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  //conn.on("event name", functionThatDoesSomething);  //.on is a methode as event handler
+  // connect : specific event that happens when a successful connection is made
+  conn.on("connect", () => {
+    console.log('From Client: Successfully connected to Server!'); //print msg for players
+  });
+
+  return conn;
+};
+
+// module.exports is an obj so we can export functions with key
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,26 +1,5 @@
-const net = require("net"); //Node's library
 
-// establishes a connection with the game server
-const connect = function() {
-
-  // we used Node's net library and createConnection function to create obj named conn
-  // conn obj represents the connection that you have with the server
-  const conn = net.createConnection({  
-    host: "10.0.2.15", // IP address here
-    port: "50541" // PORT number here
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  //conn.on("event name", functionThatDoesSomething);
-  // connect : specific event that happens when a successful connection is made
-  conn.on("connect", () => {
-    console.log('From Client: Successfully connected to Server!'); //print msg for players
-  });
-  
-  return conn;
-};
+const { connect } = require("./client");
 
 console.log("Connecting ...");
 connect();
