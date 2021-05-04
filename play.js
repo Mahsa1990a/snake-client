4bfca1c95ef950bb32465a47fa2ae9581a094cca
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-05-04 22:46:59 +0000

    added comments

--- play.js
+++ play.js
@@ -3,7 +3,8 @@ const { connect } = require("./client");
 const { setupInput } = require("./input");
 
 console.log("Connecting ...");
-// connect();
+// connect(); connect() returns an object that can be used to interact with the server
+// the object returned by connect() should be passed into setupInput()
 const conn = connect();//making connection
 
 setupInput(conn);
